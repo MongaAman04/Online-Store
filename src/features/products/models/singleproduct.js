bf90d076da560d422018286ee64a6211cd53f986
Aman Monga <amanmonga000@gmail.com>
2026-03-29 15:49:59 +0530

    Folder Structure updated

--- src/features/products/models/singleproduct.js
+++
@@ -1,9 +0,0 @@
-
-const [product, setProduct] = useState({
-    title: "Nike Air Force 1 '07 LV8",
-    price: 12499,
-    image: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/54a510de-a406-41b2-8d62-7f8c587c9a7e/air-force-1-07-lv8-shoes-9KwrSk.png",
-    category: "shoes",
-    description: "The radiance lives on in the Nike Air Force 1 '07 LV8...",
-    quantity: 10
-});
